2fcb9cfb31fd39540c1109a23c58eace64d89bae
intrand <intrand@users.noreply.github.com>
2022-09-23 15:22:06 -0500

    simple build script for local testing

---
+++ build-local.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ ! -f "/.dockerenv" ]; then
+    docker pull golang:latest && docker run -ti --rm -v "${PWD}":"${PWD}" --workdir "${PWD}" golang:latest
+fi;
+
+apt-get update &&
+apt-get install -yqq \
+    libx11-dev \
+    libxcursor-dev \
+    libxrandr-dev \
+    libxinerama-dev \
+    libxi-dev \
+    libglx-dev \
+    libgl-dev \
+    libxxf86vm-dev &&
+
+go build -v -ldflags "-s -w -X 'main.version=0.0.0' -X 'main.commit=dev' -X 'main.date=$(date '+%Y-%m-%d %H:%M:%S')' -X 'main.builtBy=manual'" .
+
+exit ${?}
+
+# GOOS=linux GOARCH=amd64 CGO_ENABLED=1 CC=amd64-linux-gnu-gcc CXX=amd64-linux-gnu-g++ HOST=amd64-linux-gnu go build -v
+# x86_64-pc-linux-gcc
